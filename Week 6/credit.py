3fbe9feb928509cac7158462d64b4303169e7a8c
Hasan Hakan ERDEN <81776038+Hakanerdn@users.noreply.github.com>
2023-09-29 21:29:28 +0300

    Update credit.py

--- Week 6/credit.py
+++ Week 6/credit.py
@@ -1,40 +1,45 @@
 from cs50 import get_int
-from math import floor
-
 
 def main():
-    digit1 = 0
-    digit2 = 0
-    num_digits = 0
+    # Initialize variables
     sum_of_double_odds = 0
     sum_of_evens = 0
+
+    # Get the credit card number from the user
     cc_number = get_int("Number: ")
 
-    while cc_number > 0:
-        digit2 = digit1
-        digit1 = cc_number % 10
+    # Convert the credit card number to a string to calculate its length
+    cc_str = str(cc_number)
+    num_digits = len(cc_str)
 
-        if num_digits % 2 == 0:
-            sum_of_evens += digit1
-        else:
-            multiple = 2 * digit1
+    # Loop through the digits of the credit card number
+    for i in range(num_digits):
+        # Extract the digit from the right
+        digit = int(cc_str[num_digits - 1 - i])
+
+        if i % 2 == 1:
+            # Double every other digit starting from the right
+            multiple = 2 * digit
             sum_of_double_odds += (multiple // 10) + (multiple % 10)
+        else:
+            # Sum the remaining digits
+            sum_of_evens += digit
 
-        cc_number //= 10
-        num_digits += 1
-
+    # Check if the credit card number is valid
     is_valid = (sum_of_evens + sum_of_double_odds) % 10 == 0
-    first_two_digits = (digit1 * 10) + digit2
 
-    if digit1 == 4 and num_digits >= 13 and num_digits <= 16 and is_valid:
-        print("VISA\n")
-    elif first_two_digits >= 51 and first_two_digits <= 55 and num_digits == 16 and is_valid:
-        print("MASTERCARD\n")
+    # Extract the first two digits for further validation
+    first_two_digits = int(cc_str[:2])
+
+    # Determine the card issuer and print the result
+    if cc_str[0] == '4' and (num_digits == 13 or num_digits == 16) and is_valid:
+        print("VISA")
+    elif 51 <= first_two_digits <= 55 and num_digits == 16 and is_valid:
+        print("MASTERCARD")
     elif (first_two_digits == 34 or first_two_digits == 37) and num_digits == 15 and is_valid:
-        print("AMEX\n")
+        print("AMEX")
     else:
-        print("INVALID\n")
-
+        print("INVALID")
 
 if __name__ == "__main__":
     main()
